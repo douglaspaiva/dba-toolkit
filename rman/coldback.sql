4777fc20d76024366853a39422cb2d5cca91b531
Douglas Paiva de Sousa <55005461+douglaspaiva@users.noreply.github.com>
2026-02-13 21:59:36 -0300

    Add files via upload

---
+++ rman/coldback.sql
@@ -0,0 +1,33 @@
+shutdown immediate;
+Decorrido: 00:00:00.00
+!cp /u02/oradata/O12C/system01.dbf /u03/coldbackup/O12C
+!cp /u02/oradata/O12C/tools01.dbf /u03/coldbackup/O12C
+!cp /u02/oradata/O12C/sysaux01.dbf /u03/coldbackup/O12C
+!cp /u02/oradata/O12C/undotbs01.dbf /u03/coldbackup/O12C
+!cp /u02/oradata/O12C/example01.dbf /u03/coldbackup/O12C
+!cp /u02/oradata/O12C/users01.dbf /u03/coldbackup/O12C
+!cp /u02/oradata/O12C/inv_mgmt_data01.dbf /u03/coldbackup/O12C
+!cp /u02/oradata/O12C/OMF/O12C/datafile/o1_mf_inv1_cdn31zd5_.dbf /u03/coldbackup/O12C
+!cp /u02/oradata/O12C/OMF/O12C/datafile/o1_mf_inv2_cdn320yf_.dbf /u03/coldbackup/O12C
+!cp /u02/oradata/O12C/tools_comp01.dbf /u03/coldbackup/O12C
+Decorrido: 00:00:00.01
+!cp /u02/oradata/O12C/temp01.dbf /u03/coldbackup/O12C
+Decorrido: 00:00:00.01
+!cp /u02/oradata/O12C/redo03.log /u03/coldbackup/O12C
+!cp /u02/oradata/O12C/redo02.log /u03/coldbackup/O12C
+!cp /u02/oradata/O12C/redo01.log /u03/coldbackup/O12C
+!cp /u03/O12C/redo01.log /u03/coldbackup/O12C
+!cp /u03/O12C/redo02.log /u03/coldbackup/O12C
+!cp /u03/O12C/redo03.log /u03/coldbackup/O12C
+!cp /u02/oradata/O12C/redo04.log /u03/coldbackup/O12C
+!cp /u03/O12C/redo04.log /u03/coldbackup/O12C
+!cp /u02/oradata/O12C/redo05.log /u03/coldbackup/O12C
+!cp /u03/O12C/redo05.log /u03/coldbackup/O12C
+!cp /u02/oradata/O12C/redo06.log /u03/coldbackup/O12C
+!cp /u03/O12C/redo06.log /u03/coldbackup/O12C
+Decorrido: 00:00:00.00
+!cp /u02/oradata/O12C/control01.ctl /u03/coldbackup/O12C
+!cp /u03/fast_recovery_area/O12C/control02.ctl /u03/coldbackup/O12C
+Decorrido: 00:00:00.01
+startup;
+Decorrido: 00:00:00.00
